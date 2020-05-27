e84acba6c9a893dcc82a7ba4ac10c6529dc015a7
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-27 21:21:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,8 @@ def my_collect(array)
   collection = []
   i = 0 
   while i < array.length 
-    collection << yield(array[i])
+    collection << yieldarray[i]
+    i += 1 
   end 
   collection 
 end 
